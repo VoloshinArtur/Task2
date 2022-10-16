fe8f3a1dde036542e5d4a12effd5b493d6a73701
VoloshinArtur <115408461+VoloshinArtur@users.noreply.github.com>
2022-10-16 16:37:39 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main(){	
+	int a, b, c, d, e, f, B;
+	cin >> B;
+	a = B / 100000;
+	b = B / 10000 %10 ;
+	c = B / 1000 % 10;
+	d = B / 100 % 10;
+	e = B / 10 % 10;
+	f = B % 10;
+	if (a+b+c==d+e+f)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
+
